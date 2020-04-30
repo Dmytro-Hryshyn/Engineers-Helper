1a1038defd78c25bbe01096c247c54a1f07215f9
Dmytro Hrushyn <prizm.drums@gmail.com>
2020-04-30 20:59:00 +0100

    Add project files.

---
+++ Forms/MainForm.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace ModernUiDesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            this.ReferenceSubmenu_Panel = new System.Windows.Forms.Panel();
+            this.Thread_Btn = new System.Windows.Forms.Button();
+            this.ReferenceMenu_Btn = new System.Windows.Forms.Button();
+            this.HelpMenu_Btn = new System.Windows.Forms.Button();
+            this.UnitConverter_Submenu_Panel = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Speed_Converter_Btn = new System.Windows.Forms.Button();
+            this.UnitConverter_Menu_Btn = new System.Windows.Forms.Button();
+            this.LogoPanel = new System.Windows.Forms.Panel();
+            this.ChildForm_Panel = new System.Windows.Forms.Panel();
+            this.MenuPanel.SuspendLayout();
+            this.ReferenceSubmenu_Panel.SuspendLayout();
+            this.UnitConverter_Submenu_Panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.AutoScroll = true;
+            this.MenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.MenuPanel.Controls.Add(this.ReferenceSubmenu_Panel);
+            this.MenuPanel.Controls.Add(this.ReferenceMenu_Btn);
+            this.MenuPanel.Controls.Add(this.HelpMenu_Btn);
+            this.MenuPanel.Controls.Add(this.UnitConverter_Submenu_Panel);
+            this.MenuPanel.Controls.Add(this.UnitConverter_Menu_Btn);
+            this.MenuPanel.Controls.Add(this.LogoPanel);
+            this.MenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(250, 714);
+            this.MenuPanel.TabIndex = 0;
+            // 
+            // ReferenceSubmenu_Panel
+            // 
+            this.ReferenceSubmenu_Panel.AutoSize = true;
+            this.ReferenceSubmenu_Panel.Controls.Add(this.Thread_Btn);
+            this.ReferenceSubmenu_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ReferenceSubmenu_Panel.Location = new System.Drawing.Point(0, 426);
+            this.ReferenceSubmenu_Panel.Name = "ReferenceSubmenu_Panel";
+            this.ReferenceSubmenu_Panel.Size = new System.Drawing.Size(250, 50);
+            this.ReferenceSubmenu_Panel.TabIndex = 1;
+            // 
+            // Thread_Btn
+            // 
+            this.Thread_Btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Thread_Btn.FlatAppearance.BorderSize = 0;
+            this.Thread_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Thread_Btn.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Thread_Btn.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Thread_Btn.Location = new System.Drawing.Point(0, 0);
+            this.Thread_Btn.Name = "Thread_Btn";
+            this.Thread_Btn.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
+            this.Thread_Btn.Size = new System.Drawing.Size(250, 50);
+            this.Thread_Btn.TabIndex = 7;
+            this.Thread_Btn.Text = "Thread";
+            this.Thread_Btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Thread_Btn.UseVisualStyleBackColor = true;
+            // 
+            // ReferenceMenu_Btn
+            // 
+            this.ReferenceMenu_Btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ReferenceMenu_Btn.FlatAppearance.BorderSize = 0;
+            this.ReferenceMenu_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ReferenceMenu_Btn.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReferenceMenu_Btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ReferenceMenu_Btn.Image = ((System.Drawing.Image)(resources.GetObject("ReferenceMenu_Btn.Image")));
+            this.ReferenceMenu_Btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ReferenceMenu_Btn.Location = new System.Drawing.Point(0, 376);
+            this.ReferenceMenu_Btn.Name = "ReferenceMenu_Btn";
+            this.ReferenceMenu_Btn.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.ReferenceMenu_Btn.Size = new System.Drawing.Size(250, 50);
+            this.ReferenceMenu_Btn.TabIndex = 3;
+            this.ReferenceMenu_Btn.Text = "Reference";
+            this.ReferenceMenu_Btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ReferenceMenu_Btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.ReferenceMenu_Btn.UseVisualStyleBackColor = true;
+            // 
+            // HelpMenu_Btn
+            // 
+            this.HelpMenu_Btn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.HelpMenu_Btn.FlatAppearance.BorderSize = 0;
+            this.HelpMenu_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HelpMenu_Btn.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelpMenu_Btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.HelpMenu_Btn.Location = new System.Drawing.Point(0, 664);
+            this.HelpMenu_Btn.Name = "HelpMenu_Btn";
+            this.HelpMenu_Btn.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.HelpMenu_Btn.Size = new System.Drawing.Size(250, 50);
+            this.HelpMenu_Btn.TabIndex = 2;
+            this.HelpMenu_Btn.Text = "Help";
+            this.HelpMenu_Btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.HelpMenu_Btn.UseVisualStyleBackColor = true;
+            // 
+            // UnitConverter_Submenu_Panel
+            // 
+            this.UnitConverter_Submenu_Panel.AutoSize = true;
+            this.UnitConverter_Submenu_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.UnitConverter_Submenu_Panel.Controls.Add(this.button4);
+            this.UnitConverter_Submenu_Panel.Controls.Add(this.button3);
+            this.UnitConverter_Submenu_Panel.Controls.Add(this.button1);
+            this.UnitConverter_Submenu_Panel.Controls.Add(this.button2);
+            this.UnitConverter_Submenu_Panel.Controls.Add(this.Speed_Converter_Btn);
+            this.UnitConverter_Submenu_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.UnitConverter_Submenu_Panel.Location = new System.Drawing.Point(0, 126);
+            this.UnitConverter_Submenu_Panel.Name = "UnitConverter_Submenu_Panel";
+            this.UnitConverter_Submenu_Panel.Size = new System.Drawing.Size(250, 250);
+            this.UnitConverter_Submenu_Panel.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(0, 200);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button4.Size = new System.Drawing.Size(250, 50);
+            this.button4.TabIndex = 6;
+            this.button4.Text = " Angle";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(0, 150);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button3.Size = new System.Drawing.Size(250, 50);
+            this.button3.TabIndex = 5;
+            this.button3.Text = " Temperature";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 100);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(250, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = " Waight && Mass";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(0, 50);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.button2.Size = new System.Drawing.Size(250, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = " Lenth";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Speed_Converter_Btn
+            // 
+            this.Speed_Converter_Btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Speed_Converter_Btn.FlatAppearance.BorderSize = 0;
+            this.Speed_Converter_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Speed_Converter_Btn.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Speed_Converter_Btn.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Speed_Converter_Btn.Image = ((System.Drawing.Image)(resources.GetObject("Speed_Converter_Btn.Image")));
+            this.Speed_Converter_Btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Speed_Converter_Btn.Location = new System.Drawing.Point(0, 0);
+            this.Speed_Converter_Btn.Name = "Speed_Converter_Btn";
+            this.Speed_Converter_Btn.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.Speed_Converter_Btn.Size = new System.Drawing.Size(250, 50);
+            this.Speed_Converter_Btn.TabIndex = 2;
+            this.Speed_Converter_Btn.Text = " Speed";
+            this.Speed_Converter_Btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Speed_Converter_Btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Speed_Converter_Btn.UseVisualStyleBackColor = true;
+            // 
+            // UnitConverter_Menu_Btn
+            // 
+            this.UnitConverter_Menu_Btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.UnitConverter_Menu_Btn.FlatAppearance.BorderSize = 0;
+            this.UnitConverter_Menu_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UnitConverter_Menu_Btn.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UnitConverter_Menu_Btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.UnitConverter_Menu_Btn.Image = ((System.Drawing.Image)(resources.GetObject("UnitConverter_Menu_Btn.Image")));
+            this.UnitConverter_Menu_Btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.UnitConverter_Menu_Btn.Location = new System.Drawing.Point(0, 76);
+            this.UnitConverter_Menu_Btn.Name = "UnitConverter_Menu_Btn";
+            this.UnitConverter_Menu_Btn.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.UnitConverter_Menu_Btn.Size = new System.Drawing.Size(250, 50);
+            this.UnitConverter_Menu_Btn.TabIndex = 1;
+            this.UnitConverter_Menu_Btn.Text = " Unit Converter";
+            this.UnitConverter_Menu_Btn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.UnitConverter_Menu_Btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.UnitConverter_Menu_Btn.UseVisualStyleBackColor = true;
+            // 
+            // LogoPanel
+            // 
+            this.LogoPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.LogoPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LogoPanel.Location = new System.Drawing.Point(0, 0);
+            this.LogoPanel.Name = "LogoPanel";
+            this.LogoPanel.Size = new System.Drawing.Size(250, 76);
+            this.LogoPanel.TabIndex = 1;
+            // 
+            // ChildForm_Panel
+            // 
+            this.ChildForm_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
+            this.ChildForm_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ChildForm_Panel.Location = new System.Drawing.Point(250, 0);
+            this.ChildForm_Panel.Name = "ChildForm_Panel";
+            this.ChildForm_Panel.Size = new System.Drawing.Size(733, 714);
+            this.ChildForm_Panel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 714);
+            this.Controls.Add(this.ChildForm_Panel);
+            this.Controls.Add(this.MenuPanel);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MenuPanel.ResumeLayout(false);
+            this.MenuPanel.PerformLayout();
+            this.ReferenceSubmenu_Panel.ResumeLayout(false);
+            this.UnitConverter_Submenu_Panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MenuPanel;
+        private System.Windows.Forms.Panel UnitConverter_Submenu_Panel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Speed_Converter_Btn;
+        private System.Windows.Forms.Button UnitConverter_Menu_Btn;
+        private System.Windows.Forms.Panel LogoPanel;
+        private System.Windows.Forms.Button HelpMenu_Btn;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel ReferenceSubmenu_Panel;
+        private System.Windows.Forms.Button Thread_Btn;
+        private System.Windows.Forms.Button ReferenceMenu_Btn;
+        private System.Windows.Forms.Panel ChildForm_Panel;
+    }
+}
+
---
+++ Forms/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Forms.UnitConverter;
+using ModernUiDesign.Controls;
+
+namespace ModernUiDesign
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();  
+            
+            SubMenuContol unitConverterSubcontol = new SubMenuContol(UnitConverter_Menu_Btn,UnitConverter_Submenu_Panel);
+            Speed_Converter_Btn.Click += (sender, EventArgs) => unitConverterSubcontol.OpenChildForm(new Speed(), ChildForm_Panel);
+            SubMenuContol referenceSubControl = new SubMenuContol(ReferenceMenu_Btn, ReferenceSubmenu_Panel);
+
+
+        }
+
+    }
+}
+
