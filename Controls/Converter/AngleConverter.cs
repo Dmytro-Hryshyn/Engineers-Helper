c1bd101e299280fb4ed659b93d1d3a66575a3e81
Dmytro Hrushyn <prizm.drums@gmail.com>
2020-05-13 20:17:35 +0100

    Angle Converter finished

--- Controls/Converter/AngleConverter.cs
+++ Controls/Converter/AngleConverter.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace ModernUiDesign.Controls.Converter
 {
@@ -16,9 +13,34 @@ namespace ModernUiDesign.Controls.Converter
 
         public string ResultString { get; private set; }
 
+        enum AngleUnit { Degree, Radian, Gradian }
+
+        public AngleConverter(decimal fromUnitValue, string fromUnitName, string toUnitName)
+        {
+            FromUnitValue = fromUnitValue;
+            FromUnitName = fromUnitName;
+            ToUnitName = toUnitName;
+            ResultString = $"{fromUnitName}-{toUnitName}";
+        }
+
         public string GetUnswer()
         {
-            throw new NotImplementedException();
+            Hashtable angleHash = new Hashtable();
+            //Degree to another Unit
+            angleHash.Add($"{AngleUnit.Degree}-{AngleUnit.Radian}", FromUnitValue * (decimal)(Math.PI / 180));
+            angleHash.Add($"{AngleUnit.Degree}-{AngleUnit.Gradian}", FromUnitValue * (200m / 180m));
+            //Radian to another unit
+            angleHash.Add($"{AngleUnit.Radian}-{AngleUnit.Degree}", FromUnitValue * (decimal)(180 / Math.PI));
+            angleHash.Add($"{AngleUnit.Radian}-{AngleUnit.Gradian}", FromUnitValue * (decimal)(200 / Math.PI));
+            //Gradian to another unit
+            angleHash.Add($"{AngleUnit.Gradian}-{AngleUnit.Degree}", FromUnitValue * (180m / 200m));
+            angleHash.Add($"{AngleUnit.Gradian}-{AngleUnit.Radian}", FromUnitValue * (decimal)(Math.PI / 200));
+
+            if (angleHash.ContainsKey(ResultString))
+            {
+                angleHash[ResultString].ToString();
+            }
+            return "0.error";
         }
     }
 }
