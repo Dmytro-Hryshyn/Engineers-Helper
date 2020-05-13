d0df0866f53ded4d264ab35827f1f184a3b97a06
Dmytro Hrushyn <prizm.drums@gmail.com>
2020-05-13 19:59:10 +0100

    AngleConverter Started

---
+++ Controls/Converter/AngleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernUiDesign.Controls.Converter
+{
+    public class AngleConverter : IConverter
+    {
+        public decimal FromUnitValue { get; private set; }
+
+        public string FromUnitName { get; private set; }
+
+        public string ToUnitName { get; private set; }
+
+        public string ResultString { get; private set; }
+
+        public string GetUnswer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
