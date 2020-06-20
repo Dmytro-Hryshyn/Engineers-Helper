ac4ff2f663e12a265b7271958d31e912ddc23549
Dmytro Hrushyn <prizm.drums@gmail.com>
2020-06-20 16:52:37 +0100

    Side Formula adjustment (Rectangle form)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ModernUiDesign")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dmytro Hryshyn Software")]
 [assembly: AssemblyProduct("Engineer's Helper")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
