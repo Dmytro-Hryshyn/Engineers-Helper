a6e8bece5128d845cba7c05c7a2f94fdf5f3dd68
Dmytro Hryshyn <Prizm.drums@gmail.com>
2020-07-19 21:47:02 +0100

    Weight And Mass converter refactored

--- Forms/UnitConverter/WeightAndMass.cs
+++ Forms/UnitConverter/WeightAndMass.cs
@@ -1,34 +1,26 @@
 ﻿using Forms.UnitConverter;
-using Unit.Converter;
 using System.Windows.Forms;
+using Unit.Converter;
 
 namespace Engineers_Helper.Forms.UnitConverter
 {
     public partial class WeightAndMass : Speed
     {
-        public string ErrorText { get; } = "Error, try select enother weight unit";
-
         public WeightAndMass()
         {
             InitializeComponent();
-            
         }
 
-        
         protected override void Calculate()
         {
-            ErrorCall call = DisplayError;
-            if (textBox_FromUnit.Text!=string.Empty)
+            if (textBox_FromUnit.Text != string.Empty)
             {
                 WeightAndMassConverter weightAndMassConverter = new WeightAndMassConverter(decimal.Parse(textBox_FromUnit.Text), comboBox_FromUnit.Text, comboBox_ToUnit.Text);
-                textBox_ToUnit.Text = weightAndMassConverter.GetUnswer(call, ErrorText).ToString("0.##########");
-               
+                textBox_ToUnit.Text = weightAndMassConverter.ConvertUnit().ToString("0.##########");
             }
-            
         }
         protected override void SetComboBox()
         {
-
             comboBox_FromUnit.DataSource = new BindingSource(WeightAndMassConverter.NiceComboboxData(), null);
             comboBox_FromUnit.DisplayMember = "Key";
             comboBox_FromUnit.ValueMember = "Value";
@@ -36,12 +28,6 @@ namespace Engineers_Helper.Forms.UnitConverter
             comboBox_ToUnit.DataSource = new BindingSource(WeightAndMassConverter.NiceComboboxData(), null);
             comboBox_ToUnit.DisplayMember = "Key";
             comboBox_ToUnit.ValueMember = "Value";
-
-        }
-
-        protected override void DisplayError(string ErrorText)
-        {
-            base.DisplayError(this.ErrorText);
         }
     }
 }
